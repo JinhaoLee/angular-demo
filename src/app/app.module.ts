446a40e4b87f6cb5c84b22c1ad6bce77fe23da6f
Jinhao Li <jinhao@Jinhaos-MacBook-Pro.local>
2019-05-30 15:59:38 +1000

    init demo project

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from './data-table/data-table.component';
+import {
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent, DataTableComponent, MainNavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/data-table/data-table.component.ts
@@ -0,0 +1,29 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { DataTableDataSource } from './data-table-datasource';
+import { TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: DataTableDataSource;
+  isLoaded = false;
+
+  /** Columns displayed in the table. */
+  displayedColumns = ['userId', 'id', 'title', 'completed'];
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(res => {
+      this.dataSource = new DataTableDataSource(this.paginator, this.sort, res);
+      console.log(this.dataSource);
+    });
+    // this.dataSource = new DataTableDataSource(this.paginator, this.sort, this.todoService, this.dataLoaded);
+  }
+}
